1b878457a8dd895a513f1ecece6b233b9bf92971
Marco Bonfiglio <mbonfiglio@mmul.it>
2020-04-09 13:20:47 +0200

    aggiunta make_report.sh: helper per lanciare openvasreporting

---
+++ make_report.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This is only an helper script
+# Launch openvasreporting to create a single report of each format from all the file # found in XML directory.
+
+# Register local path, to make XML and REPORTS relative to this
+current_dir="$( readlink -e "$( dirname "${0}" )" )"
+xml_dir="${current_dir}/XML"
+reports_dir="${current_dir}/REPORTS"
+openvasreporting_dir="${current_dir}/openvasreporting"
+virtualenv_dir="${current_dir}/virtualenv"
+
+# First: if not in virtualenv, activate it
+if ! env | grep -q VIRTUAL_ENV
+then
+# shellcheck source=virtualenv/bin/activate
+	source "${virtualenv_dir}/bin/activate"
+fi
+# Enter the directory for openvasreporting
+cd "${openvasreporting_dir}" || exit 1
+# launch
+python -m openvasreporting -i "${xml_dir}"/*.xml -o "${reports_dir}/report" -f 'csv'  "$@"
+python -m openvasreporting -i "${xml_dir}"/*.xml -o "${reports_dir}/report" -f 'xlsx' "$@"
+python -m openvasreporting -i "${xml_dir}"/*.xml -o "${reports_dir}/report" -f 'docx' "$@"
